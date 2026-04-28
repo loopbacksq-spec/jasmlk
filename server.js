228dd7360eb34a918198507aa624961a2794dc3a
loopbacksq-spec <loopbacksq@gmail.com>
2026-04-28 20:17:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,51 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname));
 
 let waitingPlayer = null;
 
 io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
+    // Глобальный чат
+    socket.on('send_msg', (data) => {
+        // Рассылаем всем, клиент сам оставит только 5 последних
+        io.emit('broadcast_msg', data);
+    });
 
-    socket.on('findMatch', (data) => {
-        if (waitingPlayer) {
-            // Создаем комнату для игры
-            const roomName = `room_${waitingPlayer.id}_${socket.id}`;
-            socket.join(roomName);
-            waitingPlayer.join(roomName);
-
-            io.to(roomName).emit('matchFound', {
-                players: [
-                    {id: waitingPlayer.id, nick: waitingPlayer.nick, elo: waitingPlayer.elo},
-                    {id: socket.id, nick: data.nick, elo: data.elo}
-                ]
-            });
+    // Поиск матча
+    socket.on('find_match', (userData) => {
+        if (waitingPlayer && waitingPlayer.id !== socket.id) {
+            const opponent = waitingPlayer;
             waitingPlayer = null;
+            
+            const roomId = `room_${socket.id}_${opponent.id}`;
+            socket.join(roomId);
+            opponent.join(roomId);
+
+            io.to(roomId).emit('match_start', {
+                room: roomId,
+                opp: { nick: opponent.nick, elo: opponent.elo },
+                you: { nick: socket.nick, elo: socket.elo }
+            });
         } else {
-            socket.nick = data.nick;
-            socket.elo = data.elo;
+            socket.nick = userData.nick;
+            socket.elo = userData.elo;
             waitingPlayer = socket;
         }
+    });
+
+    socket.on('update_score', (data) => {
+        socket.to(data.room).emit('opp_score', data.score);
+    });
+
+    socket.on('disconnect', () => {
+        if (waitingPlayer && waitingPlayer.id === socket.id) waitingPlayer = null;
     });
 });
 
