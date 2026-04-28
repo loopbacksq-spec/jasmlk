a00c7ae90fb9586685cfbc6eb82b37ff7909e509
loopbacksq-spec <loopbacksq@gmail.com>
2026-04-28 20:05:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
-// Добавь это в существующий server.js в блок io.on('connection')
-socket.on('callUser', (data) => {
-    const target = users.get(data.userToCall.toString());
-    if (target) {
-        io.to(target).emit('incomingCall', { signal: data.signalData, from: data.from });
-    }
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname + '/'));
+
+let waitingPlayer = null;
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on('findMatch', (data) => {
+        if (waitingPlayer) {
+            // Создаем комнату для игры
+            const roomName = `room_${waitingPlayer.id}_${socket.id}`;
+            socket.join(roomName);
+            waitingPlayer.join(roomName);
+
+            io.to(roomName).emit('matchFound', {
+                players: [
+                    {id: waitingPlayer.id, nick: waitingPlayer.nick, elo: waitingPlayer.elo},
+                    {id: socket.id, nick: data.nick, elo: data.elo}
+                ]
+            });
+            waitingPlayer = null;
+        } else {
+            socket.nick = data.nick;
+            socket.elo = data.elo;
+            waitingPlayer = socket;
+        }
+    });
 });
 
-socket.on('acceptCall', (data) => {
-    const target = users.get(data.to.toString());
-    if (target) {
-        io.to(target).emit('callAccepted', data.signal);
-    }
-});
-
-socket.on('endCall', (data) => {
-    const target = users.get(data.to.toString());
-    if (target) io.to(target).emit('callEnded');
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
